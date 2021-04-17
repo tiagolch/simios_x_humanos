fbf981b2ec5a2ee5bb67c3e461bcc94881811e1d
Tiago Chaves <tiagodevch@gmail.com>
2021-04-16 21:59:40 -0300

    Criado models e configurado admin

---
+++ api/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import dna, simiosXHumanos
+
+
+admin.site.register(dna)
+admin.site.register(simiosXHumanos)
+
+
+
+
+
+
+
---
+++ api/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class dna(models.Model):
+    dna = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.dna
+
+    class Meta:
+        verbose_name_plural = 'DNAs'
+
+
+class simiosXHumanos(models.Model):
+    simios = models.PositiveIntegerField(default=0)
+    humanos = models.PositiveIntegerField(default=0)
+    ratio = models.DecimalField(max_digits=3, decimal_places=1, default=00.0)
+
+    def __str__(self):
+        return str(self.ratio)
+
+    class Meta:
+        verbose_name_plural = 'Simios x Humanos'
+
